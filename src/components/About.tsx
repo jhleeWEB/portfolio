8bee758d8991d7d6b23f1f394c8e68f39276d69d
jhlee <jhlee89898989@gmail.com>
2025-03-18 21:24:34 +0900

    add 각 경로 페이지 컴포넌트 추가

---
+++ src/components/About.tsx
@@ -0,0 +1,13 @@
+import { styled } from 'styled-components';
+
+const Wrapper = styled.div(({}) => ({}));
+
+const About = () => {
+	return (
+		<Wrapper>
+			<h1>About me</h1>
+		</Wrapper>
+	);
+};
+
+export default About;
---
+++ src/components/Career.tsx
@@ -0,0 +1,13 @@
+import { styled } from 'styled-components';
+
+const Wrapper = styled.div(({}) => ({}));
+
+const Career = () => {
+	return (
+		<Wrapper>
+			<h1>Career</h1>
+		</Wrapper>
+	);
+};
+
+export default Career;
