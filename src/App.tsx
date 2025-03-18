fb8cb33229998057165b50b2df2480fbdce29c30
jhlee <jhlee89898989@gmail.com>
2025-03-18 21:23:34 +0900

    add react-router 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
+import { Routes, Route } from 'react-router';
+import { PATHNAME } from './consts/consts';
+
 const App = () => {
-	return <h1>Hello, world!</h1>;
+	return (
+		<Routes>
+			<Route path={PATHNAME.HOME} />
+			<Route path={PATHNAME.CAREER} />
+			<Route path={PATHNAME.ABOUT} />
+		</Routes>
+	);
 };
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from 'styled-components';
 import theme from '@styles/theme';
+import { BrowserRouter } from 'react-router';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
@@ -10,7 +11,9 @@ const root = createRoot(rootElement);
 root.render(
 	<React.StrictMode>
 		<ThemeProvider theme={theme}>
-			<App />
+			<BrowserRouter basename='/'>
+				<App />
+			</BrowserRouter>
 		</ThemeProvider>
 	</React.StrictMode>
 );
