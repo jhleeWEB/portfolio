fcb005e0cc3e33ad63cfeba473537a9c044dc99f
jhlee <jhlee89898989@gmail.com>
2025-03-18 21:25:41 +0900

    fix Route에 react element 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router';
 import { PATHNAME } from './consts/consts';
+import Home from './components/Home';
+import Career from './components/Career';
+import About from './components/About';
 
 const App = () => {
 	return (
 		<Routes>
-			<Route path={PATHNAME.HOME} />
-			<Route path={PATHNAME.CAREER} />
-			<Route path={PATHNAME.ABOUT} />
+			<Route path={PATHNAME.HOME} element={<Home />} />
+			<Route path={PATHNAME.CAREER} element={<Career />} />
+			<Route path={PATHNAME.ABOUT} element={<About />} />
 		</Routes>
 	);
 };
