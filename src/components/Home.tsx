0add593eb1e465ce80d202406c2811fc28f5de11
jhlee <jhlee89898989@gmail.com>
2025-03-19 09:21:38 +0900

    fix container added

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,25 @@
 import { styled } from 'styled-components';
+import image from '@assets/frye.webp';
 
-const Wrapper = styled.div(({}) => ({}));
+const Wrapper = styled.div(({}) => ({
+	display: 'flex',
+}));
+const DeveloperNameContainer = styled.article``;
+const FloatingElementsContainer = styled.section``;
 
 const Home = () => {
 	return (
 		<Wrapper>
-			<h1>home</h1>
+			<DeveloperNameContainer>
+				<img src={image} />
+				<h1>개발의 흥미와 호기심을 참지 못해 돌아온 개발자, 이중훈입니다</h1>
+				<h3>React, Next.js기반 웹 4년차 프론트엔드 개발자(재외국민)입니다. </h3>
+			</DeveloperNameContainer>
+			<FloatingElementsContainer>
+				<h1>element 1</h1>
+				<h1>element 2</h1>
+				<h1>element 3</h1>
+			</FloatingElementsContainer>
 		</Wrapper>
 	);
 };
