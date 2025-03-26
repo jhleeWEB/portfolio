1845e9c1228dd163e8e09f64bfbbfaeedf25b77b
jhlee <jhlee89898989@gmail.com>
2025-03-26 09:29:03 +0900

    feature 스택 카드 추가

--- src/components/Introduction/Introduction.tsx
+++ src/components/Introduction/Introduction.tsx
@@ -2,6 +2,7 @@ import { styled } from 'styled-components';
 import Landing from './Landing';
 import MyIntro from './MyIntro';
 import MyProjects from './MyProjects';
+import MySkills from './MySkills';
 
 const Wrapper = styled.div(({}) => ({}));
 
@@ -11,6 +12,7 @@ const Introduction = () => {
 			<Landing />
 			<MyIntro />
 			<MyProjects />
+			<MySkills />
 		</Wrapper>
 	);
 };
