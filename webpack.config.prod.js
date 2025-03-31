5dda35a8a1d1a46e02d112ce900eab5e3ee9048b
jhlee <jhlee89898989@gmail.com>
2025-03-31 14:56:20 +0900

    add sitemap.xml 추가
    xml 파일 빌드 경로에 추가

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,3 +1,4 @@
+const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
@@ -53,6 +54,7 @@ module.exports = {
 		new HtmlWebpackPlugin({
 			template: './public/index.html',
 		}),
+		new CopyPlugin([{ from: './public/sitemap.xml', to: '' }]),
 	],
 	target: ['web', 'es5'],
 };
